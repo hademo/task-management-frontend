8676fab605e13a630d208a90d893b3e1331e5d3c
Fullstack Architects <fullstackarchitects@Hades-MacBook-Pro.local>
2020-09-03 21:18:01 +0200

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { TaskAPI } from "./api/task.api";
+import { TaskDTO } from "./api/dto/task.dto";
+import {
+  Grid,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import Task from "./components/Task";
+import CreateTaskModal from "./components/CreateTaskModal";
+import EditTaskModal from "./components/EditTaskModal";
 
 function App() {
+  const [tasks, setTasks] = useState<TaskDTO[]>([]);
+  const [createTaskModalOpen, setCreateTaskModalOpen] = useState(false);
+  const [updateTaskModalOpen, setUpdateTaskModalOpen] = useState(false);
+  const [taskBeingEdited, setTaskBeingEdited] = useState<undefined | TaskDTO>(
+    undefined
+  );
+
+  const addTask = (task: TaskDTO) => {
+    setTasks([...tasks, task]);
+  };
+
+  const deleteTask = (taskId: number) => {
+    setTasks(tasks.filter((x) => x.id !== taskId));
+  };
+
+  const updateTask = (task: TaskDTO) => {
+    setTasks(
+      tasks.map((x) => {
+        if (x.id === task.id) return task;
+        return x;
+      })
+    );
+  };
+
+  useEffect(() => {
+    async function fetchAll() {
+      const resp = await TaskAPI.getAll();
+
+      setTasks(resp);
+    }
+
+    fetchAll();
+  }, []);
+
+  const onTaskEditBtnClicked = (task: TaskDTO) => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CreateTaskModal
+        open={createTaskModalOpen}
+        handleClose={() => setCreateTaskModalOpen(false)}
+        onTaskCreated={addTask}
+      />
+
+      <EditTaskModal
+        data={taskBeingEdited}
+        open={updateTaskModalOpen}
+        handleClose={() => setUpdateTaskModalOpen(false)}
+        onTaskUpdate={updateTask}
+      />
+
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Task Management
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setCreateTaskModalOpen(true)}
+          >
+            Create Task
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Grid container spacing={1} style={{ padding: 10 }}>
+        {tasks.map((task) => {
+          return (
+            <Grid item xs={3}>
+              <Task
+                data={task}
+                onTaskDelete={deleteTask}
+                onTaskUpdate={(task: TaskDTO) => {
+                  setTaskBeingEdited(task);
+                  setUpdateTaskModalOpen(true);
+                }}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
     </div>
   );
 }
